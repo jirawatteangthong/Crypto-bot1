97a7ae8a3499547df865a2cc056678df8989f70e
jirawatteangthong <kodji0814814809@gmail.com>
2025-05-05 22:06:24 +0700

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -1,1 +1,8 @@
+from utils import fetch_ohlcv, detect_order_blocks, detect_fvg, detect_bos
 
+def get_h1_zones():
+    candles = fetch_ohlcv('1h')
+    ob_zones = detect_order_blocks(candles)
+    fvg_zones = detect_fvg(candles)
+    bos = detect_bos(candles)
+    return {'ob': ob_zones, 'fvg': fvg_zones, 'bos': bos}
