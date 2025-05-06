2a51559eee391c587b6bb4c24997be6090a676bf
jirawatteangthong <kodji0814814809@gmail.com>
2025-05-06 14:39:20 +0700

    Update entry.py

--- entry.py
+++ entry.py
@@ -3,12 +3,15 @@ from utils import fetch_ohlcv, calculate_macd
 def check_entry_signal(zones):
     candles = fetch_ohlcv('5m')
     price = candles[-1][4]
+    fibo = zones.get('fibo')
 
-    for ob in zones['ob']:
-        if ob['low'] <= price <= ob['high']:
-            macd, signal, hist = calculate_macd([c[4] for c in candles])
-            if macd[-2] < signal[-2] and macd[-1] > signal[-1]:
-                return {'direction': 'long', 'price': price, 'ob': ob}
-            if macd[-2] > signal[-2] and macd[-1] < signal[-1]:
-                return {'direction': 'short', 'price': price, 'ob': ob}
+    if not fibo:
+        return None
+
+    if fibo['low'] <= price <= fibo['high']:
+        macd, signal, hist = calculate_macd([c[4] for c in candles])
+        if fibo['direction'] == 'long' and macd[-2] < signal[-2] and macd[-1] > signal[-1]:
+            return {'direction': 'long', 'price': price, 'ob': fibo['swing']}
+        if fibo['direction'] == 'short' and macd[-2] > signal[-2] and macd[-1] < signal[-1]:
+            return {'direction': 'short', 'price': price, 'ob': fibo['swing']}
     return None
