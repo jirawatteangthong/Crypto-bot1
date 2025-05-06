b9e06a292138af8bbd9f31d40e54315cb5a60471
jirawatteangthong <kodji0814814809@gmail.com>
2025-05-06 21:34:54 +0700

    Update entry.py

--- entry.py
+++ entry.py
@@ -1,8 +1,18 @@
-def check_entry_signal(zones):
-    last_price = zones['fibo']['current_price']
+from utils import fetch_ohlcv
+
+def check_entry_signal(zones, trend):
+    candles = fetch_ohlcv('15m')
+    price = candles[-1][4]
+
     for ob in zones['ob']:
-        if ob['low'] <= last_price <= ob['high']:
-            return {'direction': zones['trend'], 'price': last_price, 'ob': ob, 'full_size': True}
-    if zones['fibo']['zone_61_78'][0] <= last_price <= zones['fibo']['zone_61_78'][1]:
-        return {'direction': zones['trend'], 'price': last_price, 'ob': None, 'full_size': False}
+        if ob['low'] <= price <= ob['high']:
+            return {'direction': trend, 'price': price, 'ob': ob}
+
+    fibo_zone = zones['fibo']
+    if fibo_zone['61.8'] <= price <= fibo_zone['78.6']:
+        return {
+            'direction': trend,
+            'price': price,
+            'ob': {'low': fibo_zone['61.8'], 'high': fibo_zone['78.6'], 'partial': True}
+        }
     return None
