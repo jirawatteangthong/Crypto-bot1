d7eaac2c26e51dc94b3f4b68c851d62267c2338d
jirawatteangthong <kodji0814814809@gmail.com>
2025-05-07 15:32:27 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -13,23 +13,30 @@ exchange = ccxt.okx({
 def fetch_ohlcv(tf):
     return exchange.fetch_ohlcv(SYMBOL, timeframe=tf, limit=100)
 
-def fetch_current_price():
-    ticker = exchange.fetch_ticker(SYMBOL)
-    return ticker['last']
+def detect_bos(candles):
+    highs = [c[2] for c in candles]
+    lows = [c[3] for c in candles]
+    closes = [c[4] for c in candles]
+
+    recent_high = max(highs[-10:])
+    recent_low = min(lows[-10:])
+    prev_high = max(highs[-20:-10])
+    prev_low = min(lows[-20:-10])
+
+    if closes[-1] > prev_high:
+        return 'bullish'
+    elif closes[-1] < prev_low:
+        return 'bearish'
+    return None
 
 def detect_choch(candles):
     highs = [c[2] for c in candles]
     lows = [c[3] for c in candles]
     closes = [c[4] for c in candles]
 
-    recent_high = max(highs[-20:])
-    recent_low = min(lows[-20:])
-    prev_high = max(highs[-40:-20])
-    prev_low = min(lows[-40:-20])
-
-    if closes[-1] > prev_high:
+    if closes[-1] > max(highs[-20:-10]) and closes[-2] < max(highs[-20:-10]):
         return 'bullish'
-    elif closes[-1] < prev_low:
+    elif closes[-1] < min(lows[-20:-10]) and closes[-2] > min(lows[-20:-10]):
         return 'bearish'
     return None
 
