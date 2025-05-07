f1a2cb1f0ae4f493c4c5fcdd77738725a0cd10fd
jirawatteangthong <kodji0814814809@gmail.com>
2025-05-07 20:33:00 +0700

    Update entry.py

--- entry.py
+++ entry.py
@@ -1,15 +1,16 @@
 from utils import fetch_current_price
 
-def check_entries(fibo):
+def check_entry_signals(fibo, trend):
     price = fetch_current_price()
     entries = []
+    levels = fibo['levels']
 
-    if fibo['direction'] == 'long' and fibo['levels']['61.8'] <= price <= fibo['levels']['78.6']:
-        entries.append({'direction': 'long', 'price': fibo['levels']['61.8'], 'tp': fibo['tp'], 'sl': fibo['sl']})
-        entries.append({'direction': 'long', 'price': fibo['levels']['78.6'], 'tp': fibo['tp'], 'sl': fibo['sl']})
+    if fibo['direction'] == 'long' and levels['61.8'] <= price <= levels['78.6']:
+        for lvl in ['61.8', '78.6']:
+            entries.append({'direction': 'long', 'price': levels[lvl], 'tp': fibo['tp'], 'sl': fibo['sl'], 'level': lvl})
 
-    elif fibo['direction'] == 'short' and fibo['levels']['78.6'] <= price <= fibo['levels']['61.8']:
-        entries.append({'direction': 'short', 'price': fibo['levels']['61.8'], 'tp': fibo['tp'], 'sl': fibo['sl']})
-        entries.append({'direction': 'short', 'price': fibo['levels']['78.6'], 'tp': fibo['tp'], 'sl': fibo['sl']})
+    elif fibo['direction'] == 'short' and levels['78.6'] <= price <= levels['61.8']:
+        for lvl in ['61.8', '78.6']:
+            entries.append({'direction': 'short', 'price': levels[lvl], 'tp': fibo['tp'], 'sl': fibo['sl'], 'level': lvl})
 
     return entries
