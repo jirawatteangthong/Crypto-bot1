786a2c48c923f78b6710fb1c14096c4222b4c7c2
jirawatteangthong <kodji0814814809@gmail.com>
2025-05-07 13:53:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import time
+from config import *
 from strategy import get_m15_zones
 from entry import check_entry_signal
-from order import open_trade, monitor_trade, has_any_order
+from order import open_trade, monitor_trade
 from telegram import health_check
-from config import *
+from utils import is_new_day
 
 capital = START_CAPITAL
 last_health = time.time()
+has_position = False
 
 while True:
-    if not has_any_order():
+    if not has_position:
         zones, trend = get_m15_zones()
         signal = check_entry_signal(zones, trend)
         if signal:
-            capital, _, _ = open_trade(signal, capital)
-    monitor_trade()
+            capital, has_position = open_trade(signal, capital)
+
+    has_position = monitor_trade(capital)
+
     if time.time() - last_health >= HEALTH_CHECK_HOURS * 3600:
         health_check(capital)
         last_health = time.time()
