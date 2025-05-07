5d215d207246247f23b40d835415a18629a8ac00
jirawatteangthong <kodji0814814809@gmail.com>
2025-05-07 13:51:24 +0700

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -1,8 +1,7 @@
-from utils import fetch_ohlcv, detect_order_blocks, detect_bos, draw_fibo
+from utils import fetch_ohlcv, detect_order_blocks, detect_bos
 
 def get_m15_zones():
     candles = fetch_ohlcv('15m')
-    trend, swing = detect_bos(candles)
-    fibo_zone = draw_fibo(swing, trend)
-    ob_zones = detect_order_blocks(candles, swing, trend)
-    return {'fibo': fibo_zone, 'ob': ob_zones, 'swing': swing}, trend
+    ob_zones = detect_order_blocks(candles)
+    trend = detect_bos(candles)
+    return {'ob': ob_zones}, trend
