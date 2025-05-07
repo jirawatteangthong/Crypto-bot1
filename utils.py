f2fc8a903b524dffc5fa427ad7feb0a5c37100d4
jirawatteangthong <kodji0814814809@gmail.com>
2025-05-07 15:09:59 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -13,14 +13,25 @@ exchange = ccxt.okx({
 def fetch_ohlcv(tf):
     return exchange.fetch_ohlcv(SYMBOL, timeframe=tf, limit=100)
 
-def detect_order_blocks(candles):
-    return [{'high': candles[-10][2], 'low': candles[-10][3], 'price': candles[-10][4], 'type': 'bullish'}]
+def fetch_current_price():
+    ticker = exchange.fetch_ticker(SYMBOL)
+    return ticker['last']
 
-def detect_bos(candles):
-    if candles[-1][4] > candles[-2][4]:
+def detect_choch(candles):
+    highs = [c[2] for c in candles]
+    lows = [c[3] for c in candles]
+    closes = [c[4] for c in candles]
+
+    recent_high = max(highs[-20:])
+    recent_low = min(lows[-20:])
+    prev_high = max(highs[-40:-20])
+    prev_low = min(lows[-40:-20])
+
+    if closes[-1] > prev_high:
         return 'bullish'
-    else:
+    elif closes[-1] < prev_low:
         return 'bearish'
+    return None
 
 def is_new_day():
     return datetime.datetime.utcnow().hour == 0
