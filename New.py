2f40b9fd38bb4c5ca926afe00d4fcce45a1b87e2
jirawatteangthong <kodji0814814809@gmail.com>
2025-05-13 03:16:14 +0700

    Update New.py

--- New.py
+++ New.py
@@ -1,13 +1,1 @@
-import requests
 
-TOKEN = '7752789264:AAF-0zdgHsSSYe7PS17ePYThOFP3k7AjxBY'
-CHAT_ID = '8104629569'
-MESSAGE = 'ทดสอบการแจ้งเตือนจากบอท'
-
-url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-data = {
-    "chat_id": CHAT_ID,
-    "text": MESSAGE
-}
-response = requests.post(url, data=data)
-print(response.status_code, response.text)
