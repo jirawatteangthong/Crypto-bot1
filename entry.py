1fe33898b0c372ab683762e59cfab84af2e803b5
jirawatteangthong <kodji0814814809@gmail.com>
2025-05-14 15:58:17 +0700

    Update entry.py

--- entry.py
+++ entry.py
@@ -1,14 +1,25 @@
-from utils import fetch_current_price
+from utils import fetch_current_price, fetch_ohlcv, detect_choch
 
-def check_entry_signals(fibo, trend_h1):
+def check_entry_signal(fibo):
     price = fetch_current_price()
-    entries = []
-
-    if fibo['direction'] == 'long' and fibo['levels']['61.8'] <= price <= fibo['levels']['78.6']:
-        entries.append({'direction': 'long', 'price': fibo['levels']['61.8'], 'tp': fibo['tp'], 'sl': fibo['sl'], 'level': '61.8'})
-        entries.append({'direction': 'long', 'price': fibo['levels']['78.6'], 'tp': fibo['tp'], 'sl': fibo['sl'], 'level': '78.6'})
-    elif fibo['direction'] == 'short' and fibo['levels']['78.6'] <= price <= fibo['levels']['61.8']:
-        entries.append({'direction': 'short', 'price': fibo['levels']['61.8'], 'tp': fibo['tp'], 'sl': fibo['sl'], 'level': '61.8'})
-        entries.append({'direction': 'short', 'price': fibo['levels']['78.6'], 'tp': fibo['tp'], 'sl': fibo['sl'], 'level': '78.6'})
-
-    return entries
+    if fibo['direction'] == 'long' and fibo['levels']['78.6'] <= price <= fibo['levels']['61.8']:
+        m1 = fetch_ohlcv('1m')
+        if detect_choch(m1) == 'bullish':
+            return {
+                'direction': 'long',
+                'price': price,
+                'tp': fibo['levels']['10.0'],
+                'sl': fibo['levels']['110.0'],
+                'level': 'm1-confirmed'
+            }
+    elif fibo['direction'] == 'short' and fibo['levels']['61.8'] <= price <= fibo['levels']['78.6']:
+        m1 = fetch_ohlcv('1m')
+        if detect_choch(m1) == 'bearish':
+            return {
+                'direction': 'short',
+                'price': price,
+                'tp': fibo['levels']['10.0'],
+                'sl': fibo['levels']['110.0'],
+                'level': 'm1-confirmed'
+            }
+    return None
