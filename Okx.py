0987f0120ac9aed6c473095f7486fc5d0874e927
jirawatteangthong <kodji0814814809@gmail.com>
2025-05-14 22:20:02 +0700

    Update Okx.py

--- Okx.py
+++ Okx.py
@@ -1,13 +1,1 @@
-from config import API_KEY, API_SECRET, API_PASSPHRASE
 
-def connect_okx():
-    exchange = ccxt.okx({
-        'apiKey': API_KEY,
-        'secret': API_SECRET,
-        'password': API_PASSPHRASE,
-        'enableRateLimit': True,
-        'options': {
-            'defaultType': 'future'
-        }
-    })
-    return exchange
