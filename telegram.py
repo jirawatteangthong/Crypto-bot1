c64e94caa7c9d5caddc8b509aa92986dfe632e16
jirawatteangthong <kodji0814814809@gmail.com>
2025-05-14 21:36:16 +0700

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -1,10 +1,16 @@
+# telegram.py
 import requests
 from config import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
 
 def send_message(msg):
     url = f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage'
     data = {'chat_id': TELEGRAM_CHAT_ID, 'text': msg}
+
     try:
-        requests.post(url, data=data)
-     except Exception as e:
+        response = requests.post(url, data=data)
+        if response.status_code != 200:
+            print("Telegram ส่งไม่สำเร็จ:", response.status_code, response.text)
+        else:
+            print("ส่ง Telegram สำเร็จ:", msg)
+    except Exception as e:
         print("Telegram Error:", e)
