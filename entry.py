127f4f220e3b7f578c3345d37b58e8594aa001fd
jirawatteangthong <kodji0814814809@gmail.com>
2025-05-15 00:36:06 +0700

    Update and rename entry.py to Fibonaci.py

--- entry.py
+++
@@ -1,9 +0,0 @@
-from utils import fetch_current_price
-
-def check_entry_signal(fibo, trend):
-    price = fetch_current_price()
-    if fibo['direction'] == 'long' and price <= fibo['price']:
-        return fibo
-    elif fibo['direction'] == 'short' and price >= fibo['price']:
-        return fibo
-    return None
