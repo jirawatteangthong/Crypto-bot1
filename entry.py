97a394da575a373719d439977bfdcc98db6ff7ed
jirawatteangthong <kodji0814814809@gmail.com>
2025-05-14 22:01:51 +0700

    Update entry.py

--- entry.py
+++ entry.py
@@ -1,1 +1,13 @@
+import requests
 
+TOKEN = '7752789264:AAF-0zdgHsSSYe7PS17ePYThOFP3k7AjxBY'
+CHAT_ID = '8104629569'
+
+msg = "✅ ทดสอบส่งข้อความ Telegram จากบอท"
+
+url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+data = {'chat_id': CHAT_ID, 'text': msg}
+
+response = requests.post(url, data=data)
+print(response.status_code)
+print(response.text)
