bf6206d64d60aa8949104e5d17a45422266eb433
jirawatteangthong <kodji0814814809@gmail.com>
2025-05-14 17:49:47 +0700

    Update entry.py

--- entry.py
+++ entry.py
@@ -1,16 +1,22 @@
 from utils import fetch_current_price, fetch_ohlcv, detect_choch
-from telegram import notify
+from telegram import alert_price_in_zone
 
 def check_entry_signal(fibo):
     price = fetch_current_price()
-    if fibo['direction'] == 'long' and fibo['levels']['61.8'] <= price <= fibo['levels']['78.6']:
-        candles_m1 = fetch_ohlcv('1m')[-50:]
-        if detect_choch(candles_m1) == 'bullish':
-            notify("[ENTRY SIGNAL] Price in zone, CHoCH on M1 bullish")
-            return {'direction': 'long', 'entry': price, 'tp': fibo['tp'], 'sl': fibo['sl']}
-    elif fibo['direction'] == 'short' and fibo['levels']['78.6'] <= price <= fibo['levels']['61.8']:
-        candles_m1 = fetch_ohlcv('1m')[-50:]
-        if detect_choch(candles_m1) == 'bearish':
-            notify("[ENTRY SIGNAL] Price in zone, CHoCH on M1 bearish")
-            return {'direction': 'short', 'entry': price, 'tp': fibo['tp'], 'sl': fibo['sl']}
+    in_zone = fibo['levels']['61.8'] <= price <= fibo['levels']['78.6'] if fibo['direction'] == 'long' else fibo['levels']['78.6'] <= price <= fibo['levels']['61.8']
+    if not in_zone:
+        return None
+
+    alert_price_in_zone()
+
+    m1 = fetch_ohlcv('1m')[-50:]
+    m1_choch = detect_choch(m1)
+    if (fibo['direction'] == 'long' and m1_choch == 'bullish') or (fibo['direction'] == 'short' and m1_choch == 'bearish'):
+        return {
+            'direction': fibo['direction'],
+            'price': price,
+            'tp': fibo['tp'],
+            'sl': fibo['sl'],
+            'size': 0.7
+        }
     return None
