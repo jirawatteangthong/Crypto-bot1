eca9626fac197e362077b85df37f56f9ee423510
jirawatteangthong <kodji0814814809@gmail.com>
2025-05-14 18:33:34 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
 import ccxt
-import datetime
-from config import *
+from config import API_KEY, API_SECRET, API_PASSPHRASE
 
 exchange = ccxt.okx({
     'apiKey': API_KEY,
@@ -9,33 +8,4 @@ exchange = ccxt.okx({
     'enableRateLimit': True,
     'options': {'defaultType': 'swap'}
 })
-
-def fetch_current_price():
-    ticker = exchange.fetch_ticker(SYMBOL)
-    return ticker['last']
-
-def fetch_ohlcv(tf):
-    return exchange.fetch_ohlcv(SYMBOL, timeframe=tf, limit=200)
-
-def detect_bos(candles):
-    highs = [c[2] for c in candles]
-    lows = [c[3] for c in candles]
-    closes = [c[4] for c in candles]
-    if closes[-1] > max(highs[-20:-10]):
-        return 'bullish'
-    elif closes[-1] < min(lows[-20:-10]):
-        return 'bearish'
-    return None
-
-def detect_choch(candles):
-    highs = [c[2] for c in candles]
-    lows = [c[3] for c in candles]
-    closes = [c[4] for c in candles]
-    if closes[-2] < max(highs[-20:-10]) and closes[-1] > max(highs[-20:-10]):
-        return 'bullish'
-    elif closes[-2] > min(lows[-20:-10]) and closes[-1] < min(lows[-20:-10]):
-        return 'bearish'
-    return None
-
-def is_new_day():
-    return datetime.datetime.utcnow().hour == 0
+exchange.set_leverage(20, 'BTC-USDT-SWAP')
