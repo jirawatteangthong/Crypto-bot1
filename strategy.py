f536962c49bd033633ea4fb319c81b6a6d3ed2bf
jirawatteangthong <kodji0814814809@gmail.com>
2025-05-15 01:14:36 +0700

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -1,13 +1,69 @@
-from utils import get_highs_lows, fetch_ohlcv
+from utils import fetch_ohlcv, detect_bos, detect_choch
 
-def detect_bos_and_swing(candles):
-    closes = [c['close'] for c in candles]
-    if closes[-1] > closes[-2] > closes[-3]:
-        trend = 'bullish'
-    elif closes[-1] < closes[-2] < closes[-3]:
-        trend = 'bearish'
+prev_fibo = None
+
+def get_fibo_zone():
+    global prev_fibo
+
+    h1 = fetch_ohlcv('1h')[-200:]
+    m15 = fetch_ohlcv('15m')[-100:]
+
+    trend = detect_bos(h1)
+    choch_m15 = detect_choch(m15)
+
+    if not trend or not choch_m15 or trend != choch_m15:
+        return None, trend, 'wait'
+
+    highs = [c[2] for c in h1[-70:]]
+    lows = [c[3] for c in h1[-70:]]
+    new_high = max(highs)
+    new_low = min(lows)
+
+    if trend == 'bullish':
+        if prev_fibo:
+            retrace = (fetch_current_price() - prev_fibo['levels']['100']) / (prev_fibo['levels']['0'] - prev_fibo['levels']['100'])
+            if retrace < 0.333:
+                prev_fibo['levels']['0'] = new_high
+                prev_fibo['tp'] = new_high - 0.1 * (new_high - prev_fibo['levels']['100'])
+                prev_fibo['sl'] = prev_fibo['levels']['100'] - 0.1 * (new_high - prev_fibo['levels']['100'])
+                return prev_fibo, trend, 'ok'
+            else:
+                prev_fibo['levels']['100'] = new_low
+
+        fibo = {
+            'direction': 'long',
+            'levels': {
+                '61.8': new_low + 0.618 * (new_high - new_low),
+                '78.6': new_low + 0.786 * (new_high - new_low),
+                '0': new_high,
+                '100': new_low
+            },
+            'tp': new_high - 0.1 * (new_high - new_low),
+            'sl': new_low - 0.1 * (new_high - new_low)
+        }
+
     else:
-        return None, None
+        if prev_fibo:
+            retrace = (prev_fibo['levels']['100'] - fetch_current_price()) / (prev_fibo['levels']['100'] - prev_fibo['levels']['0'])
+            if retrace < 0.333:
+                prev_fibo['levels']['0'] = new_low
+                prev_fibo['tp'] = new_low + 0.1 * (prev_fibo['levels']['100'] - new_low)
+                prev_fibo['sl'] = prev_fibo['levels']['100'] + 0.1 * (prev_fibo['levels']['100'] - new_low)
+                return prev_fibo, trend, 'ok'
+            else:
+                prev_fibo['levels']['100'] = new_high
 
-    highs, lows = get_highs_lows(candles[-20:])
-    return trend, {'high': max(highs), 'low': min(lows)}
+        fibo = {
+            'direction': 'short',
+            'levels': {
+                '61.8': new_high - 0.618 * (new_high - new_low),
+                '78.6': new_high - 0.786 * (new_high - new_low),
+                '0': new_low,
+                '100': new_high
+            },
+            'tp': new_low + 0.1 * (new_high - new_low),
+            'sl': new_high + 0.1 * (new_high - new_low)
+        }
+
+    prev_fibo = fibo
+    return fibo, trend, 'ok'
